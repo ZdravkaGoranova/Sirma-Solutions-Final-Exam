25460ece61a5da2997af03b998bfc9328a909265
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2024-09-08 02:08:33 +0300

    Create Loading Component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import './Loading.css';
+
+export default function Loading() {
+  return (
+    <>
+      <div className="loading">
+        <h4>Loading data</h4>
+        <div className="loader"></div>
+      </div>
+    </>
+  );
+}
