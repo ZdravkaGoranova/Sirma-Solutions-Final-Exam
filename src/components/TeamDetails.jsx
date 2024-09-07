ddc860cd377160f99ab24f823e3c95a874fe6ca7
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2024-09-07 20:18:40 +0300

    Update TeamDetails

--- src/components/TeamDetails.jsx
+++ src/components/TeamDetails.jsx
@@ -27,56 +27,58 @@ export default function TeamDetails() {
 
   return (
     <>
-      <h1>Team Details</h1>
+      <div className="players-list">
+        <h1>Team Details</h1>
 
-      {players.length <= 0 ? (
-        <div>
-          <h4>Loading data</h4>
-          <div className="loader"></div>
-        </div>
-      ) : (
-        <div>
-          <div className="positionFilter">
-            <label htmlFor="positionFilter">Filter by Position: </label>
-            <select
-              id="positionFilter"
-              value={selectedPosition}
-              onChange={handlePositionChange}
-            >
-              <option value="All">ALL</option>
-              <option value="GK">GOALKEEPER (GK)</option>
-              <option value="DF">DEFENDER (DF)</option>
-              <option value="MF">MIDFIELDER (MF)</option>
-              <option value="FW">FORWARD (FW)</option>
-            </select>
+        {players.length <= 0 ? (
+          <div>
+            <h4>Loading data</h4>
+            <div className="loader"></div>
           </div>
+        ) : (
+          <div>
+            <div className="positionFilter">
+              <label htmlFor="positionFilter">Filter by Position: </label>
+              <select
+                id="positionFilter"
+                value={selectedPosition}
+                onChange={handlePositionChange}
+              >
+                <option value="All">ALL</option>
+                <option value="GK">GOALKEEPER (GK)</option>
+                <option value="DF">DEFENDER (DF)</option>
+                <option value="MF">MIDFIELDER (MF)</option>
+                <option value="FW">FORWARD (FW)</option>
+              </select>
+            </div>
 
-          <table id="players">
-            <thead>
-              <tr>
-                {/* <th>ID</th> */}
-                <th>TeamNumber</th>
-                <th>FullName</th>
-                <th>Position</th>
-                <th>TeamID</th>
-              </tr>
-            </thead>
-            <tbody>
-              {filteredPlayers.map((player, index) => (
-                <tr key={index}>
-                  {/* <td>{player.ID}</td> */}
-                  <td>{player.TeamNumber}</td>
-                  <td>{player.FullName}</td>
-                  <td className={`position ${player.Position.toLowerCase()}`}>
-                    {player.Position}
-                  </td>
-                  <td>{player.TeamID}</td>
+            <table id="players">
+              <thead>
+                <tr>
+                  {/* <th>ID</th> */}
+                  <th>TeamNumber</th>
+                  <th>FullName</th>
+                  <th>Position</th>
+                  <th>TeamID</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      )}
+              </thead>
+              <tbody>
+                {filteredPlayers.map((player, index) => (
+                  <tr key={index}>
+                    {/* <td>{player.ID}</td> */}
+                    <td>{player.TeamNumber}</td>
+                    <td>{player.FullName}</td>
+                    <td className={`position ${player.Position.toLowerCase()}`}>
+                      {player.Position}
+                    </td>
+                    <td>{player.TeamID}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
     </>
   );
 }
