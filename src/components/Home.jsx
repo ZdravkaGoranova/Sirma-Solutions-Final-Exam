d9e8e0fbdb8ccd7aafcd485a3b3ddb9bb958652c
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2024-09-08 01:19:04 +0300

    Update home views

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,70 @@
 import { useEffect, useState } from 'react';
 import { loadCSV } from '../utils/csvUtils.js';
 
-
 export default function Home() {
   const [teams, setTeams] = useState([]);
+  const [matches, setMatches] = useState([]);
+  const [teamMap, setTeamMap] = useState({});
 
-    useEffect(() => {
-        async function fetchData() {
-            const data = await loadCSV('/teams.csv');
-            setTeams(data);
-        }
+  useEffect(() => {
+    async function fetchData() {
+      const matchesData = await loadCSV('/matches.csv');
+      const teamsData = await loadCSV('/teams.csv');
 
-        fetchData();
-    }, []);
+      const parsedMatches = matchesData.map((match) => {
+        const [month, day, year] = match.Date.split('/');
+        const formattedDate = new Date(`${year}-${month}-${day}`);
+        return { ...match, Date: formattedDate };
+      });
+
+      const endDate = new Date('2024-06-26');
+
+      const filteredMatches = parsedMatches.filter(
+        (match) => match.Date <= endDate,
+      );
+      console.log(filteredMatches);
+
+      const map = teamsData.reduce((acc, team) => {
+        acc[team.ID] = team.Name;
+        return acc;
+      }, {});
+      console.log(map);
+
+      setMatches(filteredMatches);
+      setTeams(teamsData);
+      setTeamMap(map);
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <h1>Home Page</h1>
-        <div>
-            <h1>Teams</h1>
-            <ul>
-                {teams.map((team, index) => (
-                    <li key={index}>{team.Name}</li>
-                ))}
-            </ul>
-        </div>
+      <div>
+        <h1>Matches</h1>
+
+        <table id="players">
+          <thead>
+            <tr>
+              <th>ATeamID</th>
+              <th>Score</th>
+              <th>BTeamID</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {matches.map((match, index) => (
+              <tr key={index}>
+                <td>{teamMap[match.ATeamID] || 'Unknown'}</td>
+                <td>{match.Score}</td>
+                <td>{teamMap[match.BTeamID] || 'Unknown'}</td>
+                <td>{match.Date.toLocaleDateString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
-
-
-
