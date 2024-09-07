121f42abf8c001045f9d377cce851f60d643b9b9
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2024-09-07 22:25:44 +0300

    Create Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
 // import { useState } from 'react'
 
-import './App.css'
+import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home.jsx';
 import MatchDetails from './components/MatchDetails.jsx';
 import TeamDetails from './components/TeamDetails.jsx';
 import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
+      {' '}
+      <Header />
       <main>
-        <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/matchDetails" element={<MatchDetails />} />
           <Route path="/teamDetails" element={<TeamDetails />} />
         </Routes>
       </main>
+      <Footer />
     </>
   );
 }
 
-export default App
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <p>© 2024 All Rights Reserved!</p>
+      </footer>
+    </>
+  );
+}
