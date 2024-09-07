d7ef9e11ecc63dc630d8413157231fb80603ff51
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2024-09-07 12:06:28 +0300

    Implement teams views into TeamDetails.age"

--- src/components/TeamDetails.jsx
+++ src/components/TeamDetails.jsx
@@ -1,7 +1,47 @@
+// import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { loadCSV } from '../utils/csvUtils.js';
+
+import './TeamDetails.css';
+
+
 export default function TeamDetails() {
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await loadCSV('/players.csv');
+      setPlayers(data);
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <>
-      <h1>TeamDetails Page</h1>
+      <h1>Team Details</h1>
+      <table id="players">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>TeamNumber</th>
+            <th>FullName</th>
+            <th>Position</th>
+            <th>TeamID</th>
+          </tr>
+        </thead>
+        <tbody>
+          {players.map((player, index) => (
+            <tr key={index}>
+              <td>{player.ID}</td>
+              <td>{player.TeamNumber}</td>
+              <td>{player.FullName}</td>
+              <td>{player.Position}</td>
+              <td>{player.TeamID}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
