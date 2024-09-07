4348fc010546b1f6ae5f5269e5ba5b3c0a293105
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2024-09-07 21:17:35 +0300

    Adding navigation into Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import './Header.css';
+import Navigation from './Navigation.jsx';
 
 export default function Header() {
   return (
     <>
       <header>
+        <Navigation/>
         <div className="hero-image"></div>
       </header>
     </>
