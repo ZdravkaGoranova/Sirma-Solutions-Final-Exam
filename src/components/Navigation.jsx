9193bcaa8d2362ea0ee9a281f62013f3294dd9ab
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2024-09-07 21:17:09 +0300

    Create Navigation bar

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+import './Navigation.css';
+
+export default function Navigation() {
+
+  return (
+    <>
+      <nav>
+       
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/matchDetails">Match Details</Link>
+          </li>
+          <li>
+            <Link to="/teamDetails">Team Details</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
