851e710bbfcad87812e7854f68c2cce355a7d293
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2024-09-07 16:45:01 +0300

    Adding  table players, loader and  filter by position into page

--- src/components/TeamDetails.jsx
+++ src/components/TeamDetails.jsx
@@ -1,47 +1,83 @@
 // import React, { useEffect, useState } from 'react';
 import { useEffect, useState } from 'react';
 import { loadCSV } from '../utils/csvUtils.js';
-
 import './TeamDetails.css';
-
 
 export default function TeamDetails() {
   const [players, setPlayers] = useState([]);
+  const [selectedPosition, setSelectedPosition] = useState('All');
 
   useEffect(() => {
     async function fetchData() {
       const data = await loadCSV('/players.csv');
       setPlayers(data);
+      console.log(data);
     }
 
     fetchData();
   }, []);
 
+  const handlePositionChange = (event) => {
+    debugger;
+    setSelectedPosition(event.target.value);
+  };
+
+  const filteredPlayers = players.filter((player) =>
+    selectedPosition === 'All' ? true : player.Position === selectedPosition,
+  );
+
   return (
     <>
       <h1>Team Details</h1>
-      <table id="players">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>TeamNumber</th>
-            <th>FullName</th>
-            <th>Position</th>
-            <th>TeamID</th>
-          </tr>
-        </thead>
-        <tbody>
-          {players.map((player, index) => (
-            <tr key={index}>
-              <td>{player.ID}</td>
-              <td>{player.TeamNumber}</td>
-              <td>{player.FullName}</td>
-              <td>{player.Position}</td>
-              <td>{player.TeamID}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+
+      {players.length <= 0 ? (
+        <div>
+          <h4>Loading data</h4>
+          <div className="loader"></div>
+        </div>
+      ) : (
+        <div>
+          <div className="positionFilter">
+            <label htmlFor="positionFilter">Filter by Position: </label>
+            <select
+              id="positionFilter"
+              value={selectedPosition}
+              onChange={handlePositionChange}
+            >
+              <option value="All">All</option>
+              <option value="GK">Goalkeeper (GK)</option>
+              <option value="DF">DEFENDER (DF)</option>
+              <option value="MF">MIDFIELDER (MF)</option>
+              <option value="FW">FORWARD (FW)</option>
+            </select>
+          </div>
+
+          <table id="players">
+            <thead>
+              <tr>
+                {/* <th>ID</th> */}
+                <th>TeamNumber</th>
+                <th>FullName</th>
+                <th>Position</th>
+                <th>TeamID</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredPlayers.map((player, index) => (
+                <tr key={index}>
+                  {/* <td>{player.ID}</td> */}
+                  <td>{player.TeamNumber}</td>
+                  <td>{player.FullName}</td>
+                  <td className={`position ${player.Position.toLowerCase()}`}>
+                    {player.Position}
+                  </td>
+                  <td>{player.TeamID}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </>
   );
 }
