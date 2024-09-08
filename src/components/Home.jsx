39fe7051e1f5ab9a241879f2507572c8e753091f
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2024-09-09 01:30:11 +0300

    Install prop-types

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,20 @@
 import { useEffect, useState } from 'react';
 import { loadCSV } from '../utils/csvUtils.js';
 
+import './Home.css';
 import Loading from './Loading.jsx';
+import GroupList from './GroupList.jsx';
 
 export default function Home() {
   const [teams, setTeams] = useState([]);
   const [matches, setMatches] = useState([]);
   const [teamMap, setTeamMap] = useState({});
+  const [groupA, setGroupA] = useState([]);
+  const [groupB, setGroupB] = useState([]);
+  const [groupC, setGroupC] = useState([]);
+  const [groupD, setGroupD] = useState([]);
+  const [groupE, setGroupE] = useState([]);
+  const [groupF, setGroupF] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
@@ -29,6 +37,29 @@ export default function Home() {
         acc[team.ID] = team.Name;
         return acc;
       }, {});
+      console.log(map);
+
+      const groups = {
+        A: [],
+        B: [],
+        C: [],
+        D: [],
+        E: [],
+        F: [],
+      };
+
+      teamsData.forEach((team) => {
+        if (groups[team.Group]) {
+          groups[team.Group].push(team.Name);
+        }
+      });
+
+      setGroupA(groups.A);
+      setGroupB(groups.B);
+      setGroupC(groups.C);
+      setGroupD(groups.D);
+      setGroupE(groups.E);
+      setGroupF(groups.F);
 
       setMatches(filteredMatches);
       setTeams(teamsData);
@@ -48,8 +79,18 @@ export default function Home() {
   return (
     <>
       <div>
-        <h1>Matches</h1>
+        <h1>Groups</h1>
 
+        <div className="groups">
+          <GroupList groupName="Group A" groupTeams={groupA} />
+          <GroupList groupName="Group B" groupTeams={groupB} />
+          <GroupList groupName="Group C" groupTeams={groupC} />
+          <GroupList groupName="Group D" groupTeams={groupD} />
+          <GroupList groupName="Group E" groupTeams={groupE} />
+          <GroupList groupName="Group F" groupTeams={groupF} />
+        </div>
+
+        <h1>Results of the matches</h1>
         {matches.length <= 0 ? (
           <Loading />
         ) : (
@@ -85,4 +126,43 @@ export default function Home() {
       </div>
     </>
   );
+}
+
+{
+  /* <h2>Groups</h2>
+
+        <div className="danger">
+          <p>
+            <strong>Group A</strong> Some text...
+          </p>
+        </div>
+
+        <div className="success">
+          <p>
+            <strong>Group B</strong> Some text...
+          </p>
+        </div>
+
+        <div className="info">
+          <p>
+            <strong>Group C</strong> Some text...
+          </p>
+        </div>
+
+        <div className="warning">
+          <p>
+            <strong>Group D</strong> Some text...
+          </p>
+        </div>
+        <div className="info">
+          <p>
+            <strong>Group E</strong> Some text...
+          </p>
+        </div>
+
+        <div className="warning">
+          <p>
+            <strong>Group F</strong> Some text...
+          </p>
+        </div> */
 }
