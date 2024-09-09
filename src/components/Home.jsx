30bf4b74df20a1d14f60328e5dcd80a3dc31f041
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2024-09-09 20:49:47 +0300

    Update the logic for calculatePoints and calculate match points

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,22 +9,6 @@ export default function Home() {
   const [matches, setMatches] = useState([]);
   const [teamMap, setTeamMap] = useState({});
 
-  const [groupA, setGroupA] = useState([]);
-  const [groupB, setGroupB] = useState([]);
-  const [groupC, setGroupC] = useState([]);
-  const [groupD, setGroupD] = useState([]);
-  const [groupE, setGroupE] = useState([]);
-  const [groupF, setGroupF] = useState([]);
-
-  const groups = {
-    A: [],
-    B: [],
-    C: [],
-    D: [],
-    E: [],
-    F: [],
-  };
-
   useEffect(() => {
     async function fetchData() {
       const matchesData = await loadCSV('/matches.csv');
@@ -44,71 +28,67 @@ export default function Home() {
 
       let teamsDetails = teamsData.reduce((acc, team) => {
         acc[team.ID] = {
-          id: team.ID,
+          id: Number(team.ID),
           name: team.Name,
+          group: team.Group,
           points: 0,
-          wins: 0,
-          draws: 0,
-          losses: 0,
         };
         return acc;
       }, {});
-      console.log(teamsDetails);
-
-      let map = teamsData.reduce((acc, team) => {
-        acc[team.Name] = {
-          id: team.ID,
-          points: 0,
-          wins: 0,
-          draws: 0,
-          losses: 0,
-        };
-        return acc;
-      }, {});
-
-      console.log(map);
-
-  
-
-      teamsData.forEach((team) => {
-        if (groups[team.Group]) {
-          groups[team.Group][team.Name] = {
-            name: team.Name,
-            ...map[team.Name],
-          };
-        }
-      });
-
-      console.log(groups);
-
-      setGroupA(groups.A);
-      setGroupB(groups.B);
-      setGroupC(groups.C);
-      setGroupD(groups.D);
-      setGroupE(groups.E);
-      setGroupF(groups.F);
 
       setMatches(filteredMatches);
       setTeams(teamsDetails);
-      setTeamMap(map);
+      setTeamMap(teamsDetails);
     }
 
     fetchData();
   }, []);
 
+  console.log(teamMap);
   console.log(matches);
 
-  const getWinner = (ATeamID, BTeamID, score) => {
-    const [aScore, bScore] = score.split('-').map(Number);
+  const calculatePoints = () => {
+    const updatedTeams = { ...teams };
 
-    if (aScore > bScore) {
-      return ATeamID;
-    } else if (bScore > aScore) {
-      return BTeamID;
-    } else if (bScore == aScore) {
-      return 'Draw';
-    }
+    matches.forEach((match) => {
+      const ATeam = updatedTeams[match.ATeamID];
+      const BTeam = updatedTeams[match.BTeamID];
+      const [aScore, bScore] = match.Score.split('-').map(Number);
+
+      if (aScore > bScore) {
+        ATeam.points += 3;
+      } else if (bScore > aScore) {
+        BTeam.points += 3;
+      } else {
+        ATeam.points += 1;
+        BTeam.points += 1;
+      }
+    });
+
+    console.log(updatedTeams);
+    return updatedTeams;
   };
+
+  useEffect(() => {
+    if (matches.length > 0 && Object.keys(teamMap).length > 0) {
+      const updatedTeams = calculatePoints();
+      if (JSON.stringify(updatedTeams) !== JSON.stringify(teams)) {
+        setTeams(updatedTeams);
+        setTeamMap(updatedTeams);
+      }
+    }
+  }, [matches, teamMap]);
+
+  const getWinner = (ATeam, BTeam, score) => {
+    const [aScore, bScore] = score.split('-').map(Number);
+    return aScore > bScore ? ATeam.name : bScore > aScore ? BTeam.name : 'Draw';
+  };
+
+  const teamsArray = Object.values(teams);
+  const result = Object.groupBy(teamsArray, ({ group }) => group);
+
+  console.log(result);
+  console.log(result.A);
 
   return (
     <>
@@ -118,12 +98,12 @@ export default function Home() {
           <Loading />
         ) : (
           <div className="groups">
-            <GroupList groupName="Group A" groupTeams={groupA} />
-            <GroupList groupName="Group B" groupTeams={groupB} />
-            <GroupList groupName="Group C" groupTeams={groupC} />
-            <GroupList groupName="Group D" groupTeams={groupD} />
-            <GroupList groupName="Group E" groupTeams={groupE} />
-            <GroupList groupName="Group F" groupTeams={groupF} />
+            <GroupList groupName="Group A" groupTeams={result.A} />
+            <GroupList groupName="Group B" groupTeams={result.B} />
+            <GroupList groupName="Group C" groupTeams={result.C} />
+            <GroupList groupName="Group D" groupTeams={result.D} />
+            <GroupList groupName="Group E" groupTeams={result.E} />
+            <GroupList groupName="Group F" groupTeams={result.F} />
           </div>
         )}
         <h1>Results of the matches</h1>
@@ -150,9 +130,9 @@ export default function Home() {
                       match.Score,
                     )}
                   </td>
-                  <td>{teamMap[match.ATeamID] || 'Unknown'}</td>
+                  <td>{teamMap[match.ATeamID].name || 'Unknown'}</td>
                   <td>{match.Score}</td>
-                  <td>{teamMap[match.BTeamID] || 'Unknown'}</td>
+                  <td>{teamMap[match.BTeamID].name || 'Unknown'}</td>
                   <td>{match.Date.toLocaleDateString()}</td>
                 </tr>
               ))}
@@ -163,40 +143,3 @@ export default function Home() {
     </>
   );
 }
-
-// const updatePoints = (winnerID, loserID, groupName) => {
-//   // if (winnerID !== 'Draw') {
-//   //   groups[groupName][teamMap[winnerID]].points += 3;
-//   //   groups[groupName][teamMap[winnerID]].wins += 1;
-//   //   groups[groupName][teamMap[loserID]].losses += 1;
-//   // } else {
-//   //   groups[groupName][teamMap[winnerID]].points += 1;
-//   //   groups[groupName][teamMap[loserID]].points += 1;
-//   //   groups[groupName][teamMap[winnerID]].draws += 1;
-//   //   groups[groupName][teamMap[loserID]].draws += 1;
-//   // }
-//   // console.log(winnerID);
-//   // console.log(loserID);
-//   // console.log(groupName);
-// };
-
-// if (resultOfMatch !== 'Draw') {
-//   // Ако resultOfMatch е число
-//   console.log('Това е число:', resultOfMatch);
-//   const teamName = map[Number(resultOfMatch)];
-//   console.log(teamName);
-// } else if (resultOfMatch === 'string') {
-//   // Ако resultOfMatch е текст
-//   console.log('Това е текст:', resultOfMatch);
-// }
-
-// if (resultOfMach === 'Draw') {
-//   groups[groupName][match.ATeamID].points += 1;
-//   groups[groupName][match.ATeamID].draws += 1;
-
-//   groups[groupName][match.BTeamID].points += 1;
-//   groups[groupName][match.BTeamID].draws += 1;
-// }
-
-// updatePoints(winnerID, match.ATeamID, match.Group);
-// updatePoints(winnerID, match.BTeamID, match.Group);
