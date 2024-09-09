163bbab56522fbdde6ae5d8831ef304deb06896b
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2024-09-09 20:50:37 +0300

    Upgrade GroupList input

--- src/components/GroupList.jsx
+++ src/components/GroupList.jsx
@@ -2,13 +2,17 @@ import PropTypes from 'prop-types';
 import './GroupList.css';
 
 export default function GroupList({ groupName, groupTeams }) {
+  console.log(groupName);
+  console.log(groupTeams);
   return (
-    <div className="columns">
+    <div className="card">
       <ul className="group">
         <li className="groupName">{groupName}</li>
-        <li className="winner">Winner: ....</li>
+        <li className="winner">Winner: ...!!!</li>
         {groupTeams.map((team, index) => (
-          <li key={index}>{team}</li>
+          <li key={index}>
+            {team.name} - {team.points} points
+          </li>
         ))}
       </ul>
     </div>
@@ -17,5 +21,10 @@ export default function GroupList({ groupName, groupTeams }) {
 
 GroupList.propTypes = {
   groupName: PropTypes.string.isRequired,
-  groupTeams: PropTypes.arrayOf(PropTypes.string).isRequired,
+  groupTeams: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      points: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
 };
