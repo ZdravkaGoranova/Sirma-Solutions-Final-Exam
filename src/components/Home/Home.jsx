b7037f5854838b2ba27792981bfcd2d33419ba76
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2024-09-10 22:55:30 +0300

    Adding error state and logic

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,7 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+
+import './Home.css';
 
 import { loadCSV } from '../../utils/csvUtils.js';
 import { calculatePoints, getWinner } from '../../utils/teamsUtils.js';
@@ -16,54 +18,94 @@ import Loading from '../Loading/Loading.jsx';
 import GroupList from '../GroupList/GroupList.jsx';
 
 export default function Home({ teams, setTeams, matches, setMatches }) {
+  const [error, setError] = useState('');
+
   useEffect(() => {
     async function fetchData() {
+      try {
+        const matchesData = await loadCSV('/matches.csv');
+        const teamsData = await loadCSV('/teams.csv');
 
-      const matchesData = await loadCSV('/matches.csv');
-      const teamsData = await loadCSV('/teams.csv');
+        console.log(matchesData);
 
-      console.log(matchesData);
+        const hasFieldsMatches = matchesData.map((mach) => Object.keys(mach));
 
-      //  hasFields;
-      const hasFieldsMatches = matchesData.map((mach) => Object.keys(mach));
+        try {
+          fieldsValidation(hasFieldsMatches);
+        } catch (err) {
+          setError('Fields validation failed: ' + err.message);
+          return;
+        }
 
-      fieldsValidation(hasFieldsMatches);
-      scoreValidation(matchesData);
-      idValidation(matchesData);
-      aTeamIDValidation(matchesData);
-      bTeamIDValidation(matchesData);
-      dateIDValidation(matchesData);
+        try {
+          scoreValidation(matchesData);
+        } catch (err) {
+          setError('Score validation failed: ' + err.message);
+          return;
+        }
 
-      const parsedMatches = matchesData.map((match) => {
-        const [month, day, year] = match.Date.split('/');
-        const formattedDate = new Date(`${year}-${month}-${day}`);
-        return { ...match, Date: formattedDate };
-      });
+        try {
+          idValidation(matchesData);
+        } catch (err) {
+          setError('ID validation failed: ' + err.message);
+          return;
+        }
 
-      const endDate = new Date('2024-06-26');
+        try {
+          aTeamIDValidation(matchesData);
+        } catch (err) {
+          setError('ATeamID validation failed: ' + err.message);
+          return;
+        }
 
-      const filteredMatches = parsedMatches.filter(
-        (match) => match.Date <= endDate,
-      );
+        try {
+          bTeamIDValidation(matchesData);
+        } catch (err) {
+          setError('BTeamID validation failed: ' + err.message);
+          return;
+        }
 
-      let teamsDetails = teamsData.reduce((acc, team) => {
-        acc[team.ID] = {
-          id: Number(team.ID),
-          name: team.Name,
-          group: team.Group,
-          points: 0,
-          wins: 0,
-          draws: 0,
-          losses: 0,
-          difference: 0,
-        };
-        return acc;
-      }, {});
+        try {
+          dateIDValidation(matchesData);
+        } catch (err) {
+          setError('Date validation failed: ' + err.message);
+          return;
+        }
 
-      const updatedTeams = calculatePoints(filteredMatches, teamsDetails);
+        const parsedMatches = matchesData.map((match) => {
+          const [month, day, year] = match.Date.split('/');
+          const formattedDate = new Date(`${year}-${month}-${day}`);
+          return { ...match, Date: formattedDate };
+        });
 
-      setMatches(filteredMatches);
-      setTeams(updatedTeams);
+        const endDate = new Date('2024-06-26');
+
+        const filteredMatches = parsedMatches.filter(
+          (match) => match.Date <= endDate,
+        );
+
+        let teamsDetails = teamsData.reduce((acc, team) => {
+          acc[team.ID] = {
+            id: Number(team.ID),
+            name: team.Name,
+            group: team.Group,
+            points: 0,
+            wins: 0,
+            draws: 0,
+            losses: 0,
+            difference: 0,
+          };
+          return acc;
+        }, {});
+
+        const updatedTeams = calculatePoints(filteredMatches, teamsDetails);
+
+        setMatches(filteredMatches);
+        setTeams(updatedTeams);
+      } catch (err) {
+        console.error('Error fetching data:', err);
+        setError('An error occurred while loading data.');
+      }
     }
 
     fetchData();
@@ -85,76 +127,89 @@ export default function Home({ teams, setTeams, matches, setMatches }) {
 
   console.log(result);
 
-  if (matches.length === 0 || teamsArray.length === 0) {
-    return <Loading />;
-  }
-
   return (
     <>
-      <div>
-        <h1>Groups</h1>
-        <div className="groups">
-          <GroupList
-            groupName="Group A"
-            groupTeams={result.A}
-            matches={matches}
-          />
-          <GroupList
-            groupName="Group B"
-            groupTeams={result.B}
-            matches={matches}
-          />
-          <GroupList
-            groupName="Group C"
-            groupTeams={result.C}
-            matches={matches}
-          />
-          <GroupList
-            groupName="Group D"
-            groupTeams={result.D}
-            matches={matches}
-          />
-          <GroupList
-            groupName="Group E"
-            groupTeams={result.E}
-            matches={matches}
-          />
-          <GroupList
-            groupName="Group F"
-            groupTeams={result.F}
-            matches={matches}
-          />
+      {error ? (
+        <div>
+          <h1>Home</h1>
+          <h3 className="error">Error: {error}</h3>
         </div>
-        <h1>Results of the matches</h1>
-        <table id="players">
-          <thead>
-            <tr>
-              <th>Winner</th>
-              <th>ATeamID</th>
-              <th>Score</th>
-              <th>BTeamID</th>
-              <th>Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            {matches.map((match, index) => (
-              <tr key={index}>
-                <td>
-                  {getWinner(
-                    teams[match.ATeamID],
-                    teams[match.BTeamID],
-                    match.Score,
-                  )}
-                </td>
-                <td>{teams[match.ATeamID].name || 'Unknown'}</td>
-                <td>{match.Score}</td>
-                <td>{teams[match.BTeamID].name || 'Unknown'}</td>
-                <td>{match.Date.toLocaleDateString()}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      ) : (
+        <div>
+          <h1>Groups</h1>
+
+          {matches.length === 0 || teamsArray.length === 0 ? (
+            <Loading />
+          ) : (
+            <div className="groups">
+              <GroupList
+                groupName="Group A"
+                groupTeams={result.A}
+                matches={matches}
+              />
+              <GroupList
+                groupName="Group B"
+                groupTeams={result.B}
+                matches={matches}
+              />
+              <GroupList
+                groupName="Group C"
+                groupTeams={result.C}
+                matches={matches}
+              />
+              <GroupList
+                groupName="Group D"
+                groupTeams={result.D}
+                matches={matches}
+              />
+              <GroupList
+                groupName="Group E"
+                groupTeams={result.E}
+                matches={matches}
+              />
+              <GroupList
+                groupName="Group F"
+                groupTeams={result.F}
+                matches={matches}
+              />
+            </div>
+          )}
+          <h1>Results of the matches</h1>
+
+          {matches.length === 0 || teamsArray.length === 0 ? (
+            <Loading />
+          ) : (
+            <table id="players">
+              <thead>
+                <tr>
+                  <th>Winner</th>
+                  <th>ATeamID</th>
+                  <th>Score</th>
+                  <th>BTeamID</th>
+                  <th>Date</th>
+                </tr>
+              </thead>
+              <tbody>
+                {matches.map((match, index) => (
+                  <tr key={index}>
+                    <td>
+                      {getWinner(
+                        teams[match.ATeamID],
+                        teams[match.BTeamID],
+                        match.Score,
+                      )}
+                    </td>
+                    <td>{teams[match.ATeamID].name || 'Unknown'}</td>
+                    <td>{match.Score}</td>
+                    <td>{teams[match.BTeamID].name || 'Unknown'}</td>
+                    <td>{match.Date.toLocaleDateString()}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
+      )}
     </>
   );
 }
