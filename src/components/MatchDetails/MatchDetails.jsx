4c2645c007d60c937bfaeafa1044ffa5c583bba6
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2024-09-11 16:54:05 +0300

    Update :adiing info for all players of Team

--- src/components/MatchDetails/MatchDetails.jsx
+++ src/components/MatchDetails/MatchDetails.jsx
@@ -11,22 +11,31 @@ export default function MatchDetails() {
   const { aTeam, bTeam, match } = location.state || {};
 
   console.log(aTeam);
-
   console.log(bTeam);
   console.log(match);
 
   const [players, setPlayers] = useState([]);
-  // const [aTeam, setATeam] = useState([]);
-  // const [bTeam, setBTeam] = useState([]);
+  const [records, setRecords] = useState([]);
+
   const [sortConfig, setSortConfig] = useState({ key: null, direction: 'asc' });
 
   useEffect(() => {
     async function fetchData() {
       const data = await loadCSV('/players.csv');
       setPlayers(data);
+      const dataRecord = await loadCSV('/records.csv');
+      setRecords(dataRecord);
     }
     fetchData();
   }, []);
+
+  records.forEach((record) => {
+    if (record.toMinutes === 'NULL') {
+      record.toMinutes = 90;
+    }
+  });
+  console.log(records);
+  console.log(players);
 
   const sortedPlayers = [...players].sort((a, b) => {
     if (sortConfig.key) {
@@ -53,6 +62,57 @@ export default function MatchDetails() {
     (player) => Number(player.TeamID) === bTeam.id,
   );
   console.log(filteredPlayersBTeam);
+
+  const playerPlayTimesForMatchID = {};
+  console.log(records);
+
+  filteredPlayersATeam.forEach((player) => {
+   
+    debugger;
+    records.forEach((record) => {
+      if (player.ID === record.PlayerID) {
+        const playTime = record.toMinutes - Number(record.fromMinutes);
+
+        if (filteredPlayersATeam[player.ID]) {
+          playerPlayTimesForMatchID[player.FullName][record.matchID] = {
+            fromMinutes: Number(record.fromMinutes),
+            toMinutes: record.toMinutes,
+            playTime: playTime,
+          };
+        }
+      }
+    });
+  });
+
+  // records.forEach((record) => {
+  //   if (record.toMinutes === 'NULL') {
+  //     record.toMinutes = 90;
+  //   }
+
+  //   const player = players.find((p) => p.ID === record.PlayerID);
+  //   if (!player) return;
+  //   const matchID = record.MatchID;
+  //   const playTime = record.toMinutes - Number(record.fromMinutes);
+
+  //   debugger;
+  //   if (!playerPlayTimesForMatchID[player.FullName]) {
+  //     playerPlayTimesForMatchID[player.FullName] = {
+  //       FullName: player.FullName,
+  //       ID: Number(player.ID),
+  //       Position: player.Position,
+  //       TeamID: Number(player.TeamID),
+  //       TeamNumber: Number(player.TeamNumber),
+  //     };
+  //   }
+  //   if (!playerPlayTimesForMatchID[player.FullName][matchID]) {
+  //     playerPlayTimesForMatchID[player.FullName][matchID] = {
+  //       fromMinutes: Number(record.fromMinutes),
+  //       toMinutes: record.toMinutes,
+  //       playTime: playTime,
+  //     };
+  //   }
+  // });
+  // console.log(playerPlayTimesForMatchID);
 
   return (
     <>
